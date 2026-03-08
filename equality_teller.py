d2b12cfcd325a126b9cba7e1fd867ffcf0f3321b
Daizen <jkcompanero@gmail.com>
2026-03-08 13:24:39 +0800

    This is for equality teller

--- equality_teller.py
+++ equality_teller.py
@@ -1,8 +1,1 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-if num1 == num2:
-    print("The numbers are equal!")
-elif num1 != num2:
-    print("The numbers are not equal!")
-else:
-    print("Invalid input")
+print("This is for equality")
