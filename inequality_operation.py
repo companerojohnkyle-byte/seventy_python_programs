a1280745d12e1ae861be59a5372cade0d6a21e8a
Daizen <jkcompanero@gmail.com>
2026-03-08 13:34:47 +0800

    This is the final outline of inequality operation. The function of this code is to display if the two input numbers are not the same

--- inequality_operation.py
+++ inequality_operation.py
@@ -1,1 +1,6 @@
-print("Inequality")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 != num2:
+    print("The numbers are not equal!")
+else:
+    print("Invalid input")
