fca9b385e7c36b3b8e7c127368110a55259e1c43
Daizen <jkcompanero@gmail.com>
2026-03-08 13:02:45 +0800

    This is the project that displays the biggest number

---
+++ bigger_number.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 > num2:
+    print("First number is  bigger: ", num1)
+elif num2 > num1:
+    print("Second number is  bigger: ", num2)
+else:
+    print("Invalid input")
