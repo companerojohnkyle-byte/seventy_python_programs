fc3a02ac58cae50f73492e26cb3677ef973ab325
Daizen <jkcompanero@gmail.com>
2026-03-08 13:11:37 +0800

    This  is  the final commit for smaller number teller

--- smaller_number.py
+++ smaller_number.py
@@ -1,1 +1,8 @@
-print("This is for smaller number")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 < num2:
+    print("First number is  smaller: ", num1)
+elif num2 < num1:
+    print("Second number is  smaller: ", num2)
+else:
+    print("Invalid input")
