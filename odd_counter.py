b4fa0b2b9cd41156514e7df253a21e0d23c708ce
Daizen <jkcompanero@gmail.com>
2026-03-09 20:31:26 +0800

    This is the final outline for odd counter. The function of this code is to display the total odd numbers in inputted numbers

--- odd_counter.py
+++ odd_counter.py
@@ -1,1 +1,6 @@
-print("This is the outline for odd numbers counter")
+odd = 0
+for i in range(10):
+    num = int(input("Enter the number: "))
+    if num % 2 != 0:
+        odd += 1
+print("Total odd numbers: ", odd)
